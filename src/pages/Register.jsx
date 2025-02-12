2fee535c83b4d46b98331919ecc3de2b42b200ef
fardin-alve <fialve2809@gmail.com>
2025-02-12 12:45:39 +0600

    local storage fixed

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -92,7 +92,7 @@ const Register = () => {
                             <input
                                 name='name'
                                 type="text"
-                                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-purple-500 focus:border-purple-500"
+                                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-purple-500 focus:border-purple-500 dark:text-black"
                                 placeholder="Enter your Name"
                             />
                         </div>
